3c453982f5fca7ae834b21859b186b58ae043924
Gustavo Souza <gtssouza94@gmail.com>
2020-07-07 22:25:07 -0300

    Configurando rotação de tela e modo fullscreen

--- app/src/main/java/com/example/appexecutandovideo/PlayerActivity.java
+++ app/src/main/java/com/example/appexecutandovideo/PlayerActivity.java
@@ -3,6 +3,7 @@ package com.example.appexecutandovideo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
@@ -14,6 +15,15 @@ public class PlayerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
+
+        //Esconde a statusBar e a barra de navegação
+        View decorView = getWindow().getDecorView();
+        int uiOp = View.SYSTEM_UI_FLAG_FULLSCREEN;//mostra fullscreen
+
+        decorView.setSystemUiVisibility(uiOp);
+
+        //esconde a actionBar
+        getSupportActionBar().hide();
 
         videoView = findViewById(R.id.videoView);
         //executar o video
